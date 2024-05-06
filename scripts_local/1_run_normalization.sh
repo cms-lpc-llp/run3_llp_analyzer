95e5f46f5490156cf99100e00f7484927f0330ca
Chang Sun <chsun@cern.ch>
2024-05-05 22:45:49 -0700

    fix norm script

--- scripts_local/1_run_normalization.sh
+++ scripts_local/1_run_normalization.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BIN=../bin/NormalizeNtuple
+BIN=../NormalizeNtuple
 TMP_PATH=/tmp/LLP_normalize_tmpfiles
 N_JOBS="$nproc"
 LUMI=1
@@ -9,11 +9,13 @@ RSYNC=
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
-        echo "Usage: $0 [options] -- [paths containing files to be combined & normalized]"
+        echo "Usage: $0 [options]"
         echo "Options:"
         echo "  -j, --jobs <number>     Number of parallel jobs, default $nproc"
         echo "  -b, --bin <path>        Path to the binary, default ../bin/NormalizeNtuple"
         echo "  -l, --lumi <number>     Integrated luminosity, default 1 (pb)"
+        echo "  -t, --temp <path>       Temporary path, default /tmp/LLP_normalize_tmpfiles"
+        echo "  -d, --data <path>       Path to the data files"
         exit 0
         ;;
     -j|--jobs)
@@ -36,9 +38,10 @@ while [[ $# -gt 0 ]]; do
         shift
         shift
         ;;
-    -|--)
-        INP_PATHS="${@:2}"
-        break
+    -d|--data)
+        DATA_PATH="$2"
+        shift
+        shift
         ;;
     -*|--*)
         echo "Unknown option $1"
@@ -55,9 +58,9 @@ echo "N_JOBS: $N_JOBS"
 echo "BIN: $BIN"
 echo "LUMI: $LUMI pb"
 echo "TMP_PATH: $TMP_PATH"
-echo "INP_PATHS: $INP_PATHS"
+echo "DATA_PATH: $DATA_PATH"
 
-for INP_PATH in $INP_PATHS; do
+for INP_PATH in $DATA_PATH; do
     if [ ! -d "$INP_PATH" ]; then
         echo INP_PATH $INP_PATH is not a directory, exiting
         exit 1
@@ -77,30 +80,35 @@ fi
 function launch {
 
     INP_PATH=$(realpath $1)
-    BIN=$2
+    BIN=$(realpath $2)
     LUMI=$3
-    IS_DATA=$(echo $INP_PATH | grep -q "Data" && echo "yes" || echo "no")
+    TMP_PATH=$4
+
+    mkdir -p $TMP_PATH
+
+    IS_DATA=$(echo $INP_PATH | grep -q "Run20[0-9]\{2\}" && echo "yes" || echo "no")
     cd $INP_PATH
     if [ -f $INP_PATH.root ]; then
         rm $INP_PATH.root
     fi
-    hadd $INP_PATH.root $(ls *.root | sort -h | tr '\n' ' ')
+    hadd $INP_PATH.root $(ls *.root | sort -h | tr '\n' ' ') >> /dev/null
 
     if [ $? -ne 0 ]; then
-        echo "Hadd failed"
+        echo "Hadd failed for $INP_PATH"
         exit 1
     fi
 
     if [ $IS_DATA == "no" ]; then
         cd $(dirname $BIN)
-        TMP_FILE=$TMP_PATH/$(basename $INP_PATH).txt
-        echo "$(basename $INP_PATH) $INP_PATH.root" > $TMP_FILE
-        echo $BIN $TMP_FILE $LUMI
+        TMP_CONF_FILE=$TMP_PATH/$(basename $INP_PATH).txt
+        echo "$(basename $INP_PATH) $INP_PATH.root" > $TMP_CONF_FILE
+        $BIN $TMP_CONF_FILE $LUMI >> $TMP_PATH/$(basename $INP_PATH).log 2>&1
         if [ $? -ne 0 ]; then
             echo "Normalization failed"
             exit 1
         fi
-        rm $TMP_FILE
+        rm $TMP_PATH/$(basename $INP_PATH).log
+        rm $TMP_CONF_FILE
         rm $INP_PATH.root
         # rm -rf $INP_PATH
     fi
@@ -108,4 +116,4 @@ function launch {
 
 export -f launch
 
-echo $INP_PATHS | parallel --progress -j $N_JOBS launch {} $BIN $LUMI
+ls $DATA_PATH | grep -v '.root$' | parallel --progress -j $N_JOBS launch $DATA_PATH/{} $BIN $LUMI $TMP_PATH
