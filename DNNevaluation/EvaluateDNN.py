96a440669d452e01acec90145c4fcd5a27b49058
Pedro J. Fernández Manteca <pedro.fernandez.manteca@gmail.com>
2024-08-16 16:48:53 +0200

    Update DNN files

--- DNNevaluation/EvaluateDNN.py
+++ DNNevaluation/EvaluateDNN.py
@@ -5,9 +5,9 @@ import argparse
 import numpy as np
 import pandas as pd
 from array import array
+import sys
 
-# python EvaluateDNN.py --in_file /eos/user/f/fernanpe/temp/ggH_HToSSTobbbb_MH-125_MS-15_CTau1000_13p6TeV_Job279_of_371.root
-# python EvaluateDNN.py --in_file /eos/user/f/fernanpe/temp/DisplacedJet-EXOCSCCluster_Run2022E-PromptReco-v1_Job87_of_126.root
+# python EvaluateDNN.py --in_file /eos/user/f/fernanpe/temp/DYto2Mu_MLL-50to120_TuneCP5_13p6TeV_powheg-pythia8_Job9_of_70.root
 
 if __name__ == '__main__':
 
@@ -17,15 +17,24 @@ if __name__ == '__main__':
 
     File = TFile.Open(args.in_file, 'update')
     tree = File.Get("MuonSystem")
+    
+    # Already filled!
+    if 'cscRechitClusterDNN_bkgMC_plusBeamHalo' in [key.GetName() for key in tree.GetListOfBranches()]:
+        sys.exit(0)
 
-    model = keras.models.load_model("training_CA0p6_NoMerging_FlatClusterSize0to500_230415.h5")
+    #model_bkgMC = keras.models.load_model("training_CA0p6_NoMerging_WeightedClusterSize_bkgMC_CSCOnly_adversarial_240505.h5")
+    model_bkgMC_plusBeamHalo = keras.models.load_model("training_CA0p6_NoMerging_WeightedClusterSize_bkgMC_CSCOnly_adversarial_PlusBeamHalo_240510.h5")
+    #model_bkgOOTData = keras.models.load_model("training_CA0p6_NoMerging_WeightedClusterSize_bkgDATA_CSCOnly_240510.h5")
 
     nEntries = tree.GetEntries()
 
-    dtRechitClusterDNN = array('f', [-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9])
-    cscRechitClusterDNN = array('f', [-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9])
-    branch1 = tree.Branch('dtRechitClusterDNN', dtRechitClusterDNN, 'dtRechitClusterDNN[30]/F')
-    branch2 = tree.Branch('cscRechitClusterDNN', cscRechitClusterDNN, 'cscRechitClusterDNN[30]/F')
+    cscRechitClusterDNN_bkgMC = array('f', [-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9])
+    cscRechitClusterDNN_bkgMC_plusBeamHalo = array('f', [-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9])
+    cscRechitClusterDNN_bkgOOTData = array('f', [-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9, -999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9])
+
+    #branch1 = tree.Branch('cscRechitClusterDNN_bkgMC', cscRechitClusterDNN_bkgMC, 'cscRechitClusterDNN_bkgMC[30]/F')
+    branch2 = tree.Branch('cscRechitClusterDNN_bkgMC_plusBeamHalo', cscRechitClusterDNN_bkgMC_plusBeamHalo, 'cscRechitClusterDNN_bkgMC_plusBeamHalo[30]/F')
+    #branch3 = tree.Branch('cscRechitClusterDNN_bkgOOTData', cscRechitClusterDNN_bkgOOTData, 'cscRechitClusterDNN_bkgOOTData[30]/F')
 
 
     for event in tree:
@@ -45,44 +54,26 @@ if __name__ == '__main__':
             frac_rw2 = (float(eval('event.cscRechitClusterNRechitChamberPlus12' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberPlus22' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberPlus32' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberPlus42' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberMinus12' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberMinus22' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberMinus32' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberMinus42' + '[' + str(i) + ']'))) / float(eval('event.cscRechitClusterSize' + '[' + str(i) + ']'))
             frac_rw3 = (float(eval('event.cscRechitClusterNRechitChamberPlus13' + '[' + str(i) + ']')) + float(eval('event.cscRechitClusterNRechitChamberMinus13' + '[' + str(i) + ']'))) / float(eval('event.cscRechitClusterSize' + '[' + str(i) + ']'))
             
-            inputs = np.array([1, eval('event.cscRechitClusterXSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterYSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterZSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterXYSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterRSpread' + '[' + str(i) + ']'), frac_s1, frac_s2, frac_s3, frac_s4, frac_rw1, frac_rw2, frac_rw3], ndmin=2)
+            inputs = np.array([eval('event.cscRechitClusterXSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterYSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterZSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterXYSpread' + '[' + str(i) + ']'), eval('event.cscRechitClusterRSpread' + '[' + str(i) + ']'),  eval('event.cscRechitClusterSkewX' + '[' + str(i) + ']'),  eval('event.cscRechitClusterSkewY' + '[' + str(i) + ']'), frac_s1, frac_s2, frac_s3, frac_s4, frac_rw1, frac_rw2, frac_rw3], ndmin=2)
             inputs[np.isnan(inputs)] = -999.9 #replace nan by -9999.9 following the training criteria
 
             # Evaluate model
 
-            cscRechitClusterDNN[i] = model.predict(inputs)[0]
-            
+            #cscRechitClusterDNN_bkgMC[i] = model_bkgMC.predict(inputs)[0]
+            cscRechitClusterDNN_bkgMC_plusBeamHalo[i] = model_bkgMC_plusBeamHalo.predict(inputs)[0]
+            #cscRechitClusterDNN_bkgOOTData[i] = model_bkgOOTData.predict(inputs)[0]
+
         # Fill untouched array elements
-
-        for i in range(eval('event.nCscRechitClusters'), len(cscRechitClusterDNN)):
-            cscRechitClusterDNN[i] = -999.9
-
-
-        # Repeat same procedure for CSC clusters
-
-        for i in range(eval('event.nDtRechitClusters')):
-
-            frac_s1 = float(eval('event.dtRechitClusterNHitStation1' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-            frac_s2 = float(eval('event.dtRechitClusterNHitStation2' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-            frac_s3 = float(eval('event.dtRechitClusterNHitStation3' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-            frac_s4 = float(eval('event.dtRechitClusterNHitStation4' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-
-            frac_rw1 = float(eval('event.dtRechitClusterNHitWheel0' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-            frac_rw2 = float(eval('event.dtRechitClusterNHitWheel1' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-            frac_rw3 = float(eval('event.dtRechitClusterNHitWheel2' + '[' + str(i) + ']')) / float(eval('event.dtRechitClusterSize' + '[' + str(i) + ']'))
-
-            inputs = np.array([0, eval('event.dtRechitClusterXSpread' + '[' + str(i) + ']'), eval('event.dtRechitClusterYSpread' + '[' + str(i) + ']'), eval('event.dtRechitClusterZSpread' + '[' + str(i) + ']'), eval('event.dtRechitClusterXYSpread' + '[' + str(i) + ']'), eval('event.dtRechitClusterRSpread' + '[' + str(i) + ']'), frac_s1, frac_s2, frac_s3, frac_s4, frac_rw1, frac_rw2, frac_rw3], ndmin=2)
-            inputs[np.isnan(inputs)] = -999.9 #replace nan by -9999.9 following the training criteria
-
-            dtRechitClusterDNN[i] = model.predict(inputs)[0]
-
-        for i in range(eval('event.nDtRechitClusters'), len(dtRechitClusterDNN)):
-            dtRechitClusterDNN[i] = -999.9
+        for i in range(eval('event.nCscRechitClusters'), len(cscRechitClusterDNN_bkgMC)):
+            #cscRechitClusterDNN_bkgMC[i] = -999.9
+            cscRechitClusterDNN_bkgMC_plusBeamHalo[i] = -999.9
+            #cscRechitClusterDNN_bkgOOTData[i] = -999.9
         
         # Fill new branches
 
-        branch1.Fill()
+        #branch1.Fill()
         branch2.Fill()
+        #branch3.Fill()
 
 
     File.Write("", TFile.kOverwrite)
--- DNNevaluation/make_list_of_files.py
+++ DNNevaluation/make_list_of_files.py
@@ -3,10 +3,13 @@ import shutil
 import glob
 
 list_dirs = [
-    '/eos/user/f/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_230412/V1p19/Data2022/v10/DisplacedJet-EXOCSCCluster_Run2022E-PromptReco-v1/',
-    '/eos/user/f/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_230412/V1p19/Data2022/v10/DisplacedJet-EXOCSCCluster_Run2022F-PromptReco-v1/',
-    '/eos/user/f/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_230412/V1p19/Data2022/v10/DisplacedJet-EXOCSCCluster_Run2022G-PromptReco-v1/',
-    '/eos/user/f/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_230412/V1p19/MC_Summer22EE/v1/sixie/v10/ggH_HToSSTobbbb_MH-125_MS-15_CTau1000_13p6TeV/',
+#    '/eos/user/f/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_240310_CSCOnly_DNNs/V1p19/'
+#    '/eos/cms/store/group/phys_smp/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_231001/V1p19/Data2023/v10/*EXOCSCCluster*'
+#    '/eos/cms/store/group/phys_smp/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_231001/V1p19/Data2023/v10/Muon1-EXOCSCCluster_Run2023D-PromptReco-v1/',
+#    '/eos/cms/store/group/phys_smp/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_231001/V1p19/Data2023/v10/Muon1-EXOCSCCluster_Run2023C-PromptReco-v2/'
+
+#    '/eos/cms/store/group/phys_smp/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_231001/V1p19/MC_Summer23/v2/sixie/v2/ggH_HToSSTobbbb_MH-125_MS-15_CTau1000_13p6TeV/'
+    '/eos/cms/store/group/phys_smp/fernanpe/displacedJetMuonAnalyzer_CA0p6_noMerging_240613_signals/V1p19_fixed/',
 ]
 
 # make list of files 
