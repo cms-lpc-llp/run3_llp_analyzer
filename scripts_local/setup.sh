6d6c0be2c569cb6112843f1d0b9ecc5841c4e5cd
christinaw97 <christina.wang@caltech.edu>
2024-10-22 18:03:46 -0700

    update

--- scripts_local/setup.sh
+++ scripts_local/setup.sh
@@ -1,4 +1,4 @@
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /cvmfs/cms.cern.ch/el9_amd64_gcc12/cms/cmssw/CMSSW_14_1_0_pre4/
+cd /cvmfs/cms.cern.ch/el8_amd64_gcc12/cms/cmssw/CMSSW_14_1_0_pre4/src/
 eval `scramv1 runtime -sh`
 cd -
