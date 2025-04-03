8cd661a734723f6ee48c2f60a390fae043a01238
christinaw97 <christina.wang@caltech.edu>
2025-04-02 17:12:36 -0700

    add analyzer for mdsnano

---
+++ analyzers/llp_MuonSystem_CA_mdsnano.h
@@ -0,0 +1,12 @@
+#ifndef DEF_llp_MuonSystem_CA_mdsnano
+#define DEF_llp_MuonSystem_CA_mdsnano
+
+#include "RazorAnalyzer.h"
+
+class llp_MuonSystem_CA_mdsnano: public RazorAnalyzerMerged {
+    public: 
+    llp_MuonSystem_CA_mdsnano(TTree *tree=0): RazorAnalyzerMerged(tree) { }
+        void Analyze(bool isData, int option, string outputFileName, string label);
+};
+
+#endif
